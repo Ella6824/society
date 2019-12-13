3c6ae108d4c40bf9b6f9f9a2bf949024daa32f6f
Ella6824 <Ella6824@github.com>
2019-12-13 21:22:22 +0800

    p23

--- src/main/java/life/ella/society/Controller/IndexController.java
+++ src/main/java/life/ella/society/Controller/IndexController.java
@@ -1,7 +1,11 @@
 package life.ella.society.Controller;
 
+import life.ella.society.dto.QuestionDTO;
+import life.ella.society.mapper.QuestionMapper;
 import life.ella.society.mapper.UserMapper;
+import life.ella.society.model.Question;
 import life.ella.society.model.User;
+import life.ella.society.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -17,8 +22,12 @@ public class IndexController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request,
+                        Model model){
         Cookie[] cookies=request.getCookies();
         if(cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
@@ -32,6 +41,8 @@ public class IndexController {
                 }
             }
         }
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
     }
 }
