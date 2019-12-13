1e94d1ec316d05cbfddb022e6a91a0a7e20c6904
Ella6824 <Ella6824@github.com>
2019-12-13 12:24:35 +0800

    :

---
+++ src/main/java/life/ella/society/SocietyApplication.java
@@ -0,0 +1,14 @@
+package life.ella.society;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class SocietyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocietyApplication.class, args);
+    }
+
+}
