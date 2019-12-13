267ae15185d87cc23378f29e321598ebd276b7f2
Ella6824 <Ella6824@github.com>
2019-12-13 14:37:39 +0800

    p12

---
+++ src/main/java/life/ella/society/Controller/AuthorizeController.java
@@ -0,0 +1,31 @@
+package life.ella.society.Controller;
+
+import life.ella.society.dto.AccessTokenDTO;
+import life.ella.society.dto.GithubUser;
+import life.ella.society.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("08af2b3aa9ac00dae532");
+        accessTokenDTO.setClient_secret("2b73c01a1379b18297e77671852ba17e930bbc77");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
