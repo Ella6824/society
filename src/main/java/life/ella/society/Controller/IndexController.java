e294893cb2cf1f97e1c954dffffc9e3ab3194e89
Ella6824 <Ella6824@github.com>
2019-12-14 22:07:43 +0800

    p26

--- src/main/java/life/ella/society/Controller/IndexController.java
+++ src/main/java/life/ella/society/Controller/IndexController.java
@@ -1,5 +1,6 @@
 package life.ella.society.Controller;
 
+import life.ella.society.dto.PaginationDTO;
 import life.ella.society.dto.QuestionDTO;
 import life.ella.society.mapper.QuestionMapper;
 import life.ella.society.mapper.UserMapper;
@@ -27,7 +28,9 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model){
+                        Model model,
+                        @RequestParam(name="page",defaultValue = "1") Integer page,
+                        @RequestParam(name="size",defaultValue = "5") Integer size){
         Cookie[] cookies=request.getCookies();
         if(cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
@@ -41,8 +44,8 @@ public class IndexController {
                 }
             }
         }
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
