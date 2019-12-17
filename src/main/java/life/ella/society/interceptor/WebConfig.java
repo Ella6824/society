1b32a25d7ded5ce1355205197ce79b642dcbf486
Ella6824 <Ella6824@github.com>
2019-12-17 20:31:52 +0800

    p31

--- src/main/java/life/ella/society/interceptor/WebConfig.java
+++ src/main/java/life/ella/society/interceptor/WebConfig.java
@@ -1,6 +1,5 @@
 package life.ella.society.interceptor;
 
-import life.ella.society.interceptor.SessionInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
